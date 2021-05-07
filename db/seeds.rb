9467932f97f138769aa0696b5502bad154628fa3
daniel gordon <danielgordon19972@gmail.com>
2021-05-07 10:41:43 +0300

    books seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require "open-uri"
+
+
+puts "creating book seeds"
+
+30.times do
+  puts "parsing API"
+  file = URI.open("https://www.googleapis.com/books/v1/volumes?q=#{Faker::Book.title}").read
+  book_response = JSON.parse(file)
+  book = book_response['items'].first['volumeInfo']
+  book
+  title = book['title']
+  puts "adding #{title} to books"
+  author = book['authors']
+  if author
+    author = author.first
+  end
+  description = book['description']
+  category = book["categories"]
+  if category
+    category = category.first
+  end
+  rating = book['averageRating']
+  image_url = book['imageLinks']['thumbnail']
+  file_image = URI.open(image_url)
+  book = Book.new(title: title, author: author, summary: description, category: category, rating: rating)
+  book.photo.attach(io: file_image, filename: "nes.png", content_type: 'image/png')
+  book.save
+  puts "saved #{title} to books"
+end
+
+puts "finished book seeds"
