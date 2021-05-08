f22ae64db7a6c1826d606a03f00b65b06cbf9cdd
daniel gordon <danielgordon19972@gmail.com>
2021-05-08 14:27:54 +0300

    added seeds for every model

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@ BookReview.destroy_all
 CatalogItem.destroy_all
 Chatroom.destroy_all
 Comment.destroy_all
+Message.destroy_all
+CatalogItem.destroy_all
+UserReview.destroy_all
+
 
 puts "creating users"
 
@@ -22,7 +26,7 @@ puts "created Shane"
 
 puts "creating book seeds"
 
-1.times do
+40.times do
   puts "parsing API"
   file = URI.open("https://www.googleapis.com/books/v1/volumes?q=#{Faker::Book.title}").read
   book_response = JSON.parse(file)
@@ -41,11 +45,13 @@ puts "creating book seeds"
   if book['averageRating']
     rating = book['averageRating']
   end
-  image_url = book['imageLinks']['thumbnail']
-  file_image = URI.open(image_url)
-  book = Book.new(title: title, author: author, summary: description, category: category, rating: rating)
-  book.photo.attach(io: file_image, filename: "nes.png", content_type: 'image/png')
-  book.save
+  new_book = Book.new(title: title, author: author, summary: description, category: category, rating: rating)
+  if book['imageLinks']['thumbnail']
+    image_url = book['imageLinks']['thumbnail']
+    file_image = URI.open(image_url)
+    new_book.photo.attach(io: file_image, filename: "nes.png", content_type: 'image/png')
+  end
+  new_book.save
   puts "saved #{title} to books"
 end
 
@@ -53,7 +59,7 @@ puts "finished book seeds"
 
 puts "creating book reviews"
 
-1.times do
+50.times do
   BookReview.create!(user: User.all.sample, book: Book.all.sample, content: Faker::Lorem.paragraph(sentence_count: rand(2..6)), rating: rand(1..5))
 end
 
@@ -63,7 +69,7 @@ puts "creating catalog items"
 
 quality_arr = ["mint condition", "pretty good", "ok", "slightly ripped", "not great"]
 
-1.times do
+50.times do
   CatalogItem.create!(quality: quality_arr.sample, user_summary: Faker::Lorem.paragraph(sentence_count: rand(2..4)), available: rand(1..2) == 1 ? true : false, user: User.all.sample, book: Book.all.sample)
 end
 
@@ -71,7 +77,7 @@ puts "finished creating catalog items"
 
 puts "creating chatrooms"
 
-1.times do
+5.times do
   user1 = User.all.sample
   user_arr = User.all.filter { |user| user != user1 }
   Chatroom.create!(user1: user1, user2: user_arr.sample)
@@ -81,7 +87,7 @@ puts "finished creating chatrooms"
 
 puts "creating comments"
 
-4.times do
+40.times do
   comment = Comment.new(user: User.all.sample, book: Book.all.sample, content: Faker::Lorem.paragraph(sentence_count: rand(2..4)))
   comment.parent = rand(1..4) == 1 ? nil : Comment.all.sample
   comment.save
@@ -89,7 +95,43 @@ end
 
 puts "finished creating comments"
 
+puts "creating messages"
 
+40.times do
+  Message.create!(user: User.all.sample, chatroom: Chatroom.all.sample, content: Faker::Lorem.paragraph(sentence_count: rand(1..4)))
+end
+
+puts "finished creating messages"
+
+puts "Creating requests"
+
+50.times do
+  status_arr = ["pending", "accepted", "declined"]
+  ci1 = CatalogItem.all.sample
+  ci_arr = CatalogItem.all.filter { |item| item != ci1 }
+  ci2 = rand(1..3) == 1 ? nil : ci_arr.sample
+  request = Request.new(user: User.all.sample, status: status_arr.sample)
+  request.buyer_item = ci1
+  request.seller_item = ci2
+  request.save
+end
+
+puts "finished creating requests"
+
+puts "creating user reviews"
+
+40.times do
+  user1 = User.all.sample
+  if user1.first_name == "Daniel"
+    puts "rating that god Daniel"
+  else
+    puts "rating that piece of shit #{user1.first_name}"
+  end
+  user_arr = User.all.filter { |user| user != user1 }
+  UserReview.create!(content: Faker::Lorem.paragraph(sentence_count: rand(1..4)), rating: rand(1..5), author: user1, receiver: user_arr.sample)
+end
+
+puts "finished creating user reviews"
 
 
 
