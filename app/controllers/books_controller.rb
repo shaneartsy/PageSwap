39d6ad7b1a335a17c889d2581cecf915f53c909c
hugo gurney <hugogurney5@gmail.com>
2021-05-09 11:55:22 +0200

    before merge

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+    authorize @book
+  end
+
+  def create
+    @book = Book.new(book_params)
+    authorize @book
+    if @book.save
+      flash[:notice] = "Book has been added"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author,
+                                 :summary, :category,
+                                 :rating)
+  end
 end
