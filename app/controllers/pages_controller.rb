e5d1319e6774dd6722bc3fa399157b0ae799507b
Shane Artsy <shaneartsy@gmail.com>
2021-05-09 14:54:01 +0300

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about_us, :faq ]
+  skip_before_action :authenticate_user!, only: [:home, :about_us, :faq]
 
   def home
     @users = User.all
