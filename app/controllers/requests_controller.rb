5f024a3845a6d74a8217d7a60dc69a6d3cec92f6
daniel gordon <danielgordon19972@gmail.com>
2021-05-11 15:04:30 +0300

    added delete button to pending

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,22 @@
 class RequestsController < ApplicationController
 
   def create
-  @request = Request.new
-     @catalog_item = CatalogItem.find(params[:catalog_item_id])
-     @request.buyer_item = @catalog_item
-     @request.user = current_user
-     @request.save
-     redirect_to user_catalog_items_path(@catalog_item.user)
+    @request = Request.new
+    @catalog_item = CatalogItem.find(params[:catalog_item_id])
+    @request.buyer_item = @catalog_item
+    @request.user = current_user
+    @request.save
+    redirect_to user_catalog_items_path(@catalog_item.user)
   end
 
   def dashboard
     @requests = Request.all
+  end
+
+  def destroy
+    request = Request.find(params[:id])
+    request.delete
+    redirect_to dashboard_path
   end
 
 end
