02423b179abba3b98a08048c724a188683c4a60c
daniel gordon <danielgordon19972@gmail.com>
2021-05-20 11:04:16 +0300

    footer stuff

--- app/controllers/catalog_items_controller.rb
+++ app/controllers/catalog_items_controller.rb
@@ -1,5 +1,6 @@
 class CatalogItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @user = User.find(params[:user_id])
     @items = @user.catalog_items
